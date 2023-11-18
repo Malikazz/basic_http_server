42266d191a4288bcdb823182d09fa39845a1333f
Paul Mana <paul@malikaz.com>
2023-11-18 16:16:14 -0700

    middle of changes send a response back now but need to detect and act on the request

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::{net::{TcpListener, TcpStream, Shutdown}, io::{Read, Write}, time::Duration};
+use std::{net::{TcpListener, TcpStream, Shutdown}, io::{Read, Write, BufWriter}, time::Duration, fs::{self, File}, error::Error};
 
 fn main() {
     let listen_address = String::from("127.0.0.1:8080");
@@ -17,9 +17,10 @@ pub fn handle_tcp_stream(mut stream: TcpStream){
     
     println!("Handling request bytes read {:?}", stream.read_to_end(&mut data));
     println!("Request data {:?}\n", byte_to_string(data));
+
+    not_found_response(&mut stream);
     
     stream.shutdown(Shutdown::Both).expect("Could not close conention");
-    stream.flush().expect("stream could not flush");
 }
 
 pub fn start_listener(address: String) -> TcpListener{
@@ -35,4 +36,35 @@ pub fn start_listener(address: String) -> TcpListener{
 pub fn byte_to_string(bytes: Vec<u8>) -> String{
     let output = String::from_utf8(bytes).expect("Found invalid UTF-8");
     output
+}
+
+pub fn parse_http_request(request: String){
+    
+}
+
+pub fn not_found_response(stream: &mut TcpStream){
+    let http_code = String::from("HTTP/1.1 200 OK\r\n\r\n");
+
+    let message = String::from("");
+   
+    stream.write(message.as_bytes()).expect("Failed to write to stream");
+}
+
+pub fn load_resource(path: String) -> Result<String, String>{
+    let file = File::open(path);
+    let mut content: String;
+    let result
+    match file {
+        Ok(temp) => temp.read_to_string(&mut content),
+        Err(error) => 
+    }
+}
+
+struct HttpRequest{
+    method: Method,
+    requested_path: String
+}
+
+enum Method {
+    GET
 }
